205a357f5b7ac768e56c047e4fad3323859484f8
ShruthiGene <shruthisub22@gmail.com>
2026-07-01 00:45:22 +0000

    Deploying to gh-pages from @ ShruthiGene/ShruthiGene.github.io@9e36bfedeea283040c548ce4822349542dccc7c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,11 +409,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
